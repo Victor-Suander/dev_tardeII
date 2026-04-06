2215753056cd08a0cb15c9cc3a6f8a5d105722f6
Victor-Suander <victors.empresa@gmail.com>
2026-04-06 17:21:09 -0300

    Estrututa de decisao II - Validaçao Texto

---
+++ validar_str.java
@@ -0,0 +1,8 @@
+void main() {
+        String input = IO.readln("Digite uma string: ");
+        if (input.isBlank()) {
+            IO.println("A string é inválida. Por favor, digite uma string não vazia.");
+        } else {
+            IO.println(input);
+        }
+    }
