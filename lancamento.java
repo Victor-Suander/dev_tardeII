d73d8568e540a30398a7a99f90d131ff1f362cef
Victor-Suander <victors.empresa@gmail.com>
2026-04-06 22:08:20 -0300

    Estrutura de decisao II - Lancamento(colocar ignicao)

--- lancamento.java
+++ lancamento.java
@@ -1,13 +1,25 @@
 void main() {
-        IO.println("Bem-vindo ao sistema de lançamento, pressione Enter para iniciar a contagem regressiva!");
-        IO.readln();
-        for (int i = 10; i >= 0; i--) {
-            IO.println(i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                IO.println("Contagem interrompida!");
-                return;
-            }
+
+    IO.println("  |-----------------------------------------------|");
+    IO.println("  |      SISTEMA DE LANÇAMENTO DE FOGUETE         |");
+    IO.println("  |-----------------------------------------------|");
+    IO.println("Bem-vindo ao sistema de lançamento!");
+    IO.println("A contagem regressiva será iniciada de 10 até 0.");
+    IO.println("Pressione Enter para iniciar...");
+
+    IO.readln();
+
+    for (int i = 10; i >= 0; i--) {
+        IO.println(i);
+
+        try {
+            Thread.sleep(1000); // pausa de 1 segundo
+        } catch (InterruptedException e) {
+            IO.println("Contagem interrompida!");
+            return;
         }
+    }
+
+    IO.println("Ignição!");
+
 }
